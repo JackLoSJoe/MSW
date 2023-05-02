c807bc8a1b02ca4ff598b7460d5fc9fa0c5ff775
Josef Nesnídal <josefnesnidal@email.cz>
2023-05-02 17:45:44 +0200

    test

---
+++ main.py
@@ -0,0 +1,8 @@
+
+print(11 / 2)           # dělení
+print(10 / 2)           # vždy vrací float
+print(11 // 2)          # celočíselné dělení
+print(-11 // 2)         # záporná čísla zaokrouhlena dolů
+print(11.0 // 2)        # je float (!)
+print(11 ** 2)          # umocňování
+print(11 % 2)           # zbytek po celočíselném dělení
