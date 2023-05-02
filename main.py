3f4f2bb50814ef39032f1d82eb1439bf99790a49
Josef Nesnídal <josefnesnidal@email.cz>
2023-05-02 18:00:22 +0200

    _

--- main.py
+++
@@ -1,8 +0,0 @@
-
-print(11 / 2)           # dělení
-print(10 / 2)           # vždy vrací float
-print(11 // 2)          # celočíselné dělení
-print(-11 // 2)         # záporná čísla zaokrouhlena dolů
-print(11.0 // 2)        # je float (!)
-print(11 ** 2)          # umocňování
-print(11 % 2)           # zbytek po celočíselném dělení
